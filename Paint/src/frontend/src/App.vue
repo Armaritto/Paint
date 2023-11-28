b4ba4ee08d162a87cc7f780d825e719bdccfe880
Armaritto <armia.joseph35@gmail.com>
2023-11-28 11:09:40 +0200

    initial commit

---
+++ Paint/src/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<Painter></Painter>
+</template>
+
+<script>
+import Painter from './components/Painter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Painter
+  }
+}
+</script>
+
+<style>
+
+</style>
